615fa4e2a8c6df7a59a57b74a65a19165af85b75
Maya Sato <maya-sato@itsol.co.jp>
2023-09-22 10:39:30 +0900

    [add] カートで管理するデータを追加

--- lib/model/data/cart_item/cart_item.dart
+++ lib/model/data/cart_item/cart_item.dart
@@ -1,6 +1,14 @@
-class CartItem {
-  final String itemId;
-  final String option;
-  final int count;
-  CartItem({required this.itemId, required this.option, required this.count});
+// ignore: unused_import
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'cart_item.freezed.dart';
+
+@freezed
+class CartItem with _$CartItem {
+  factory CartItem({
+    required String itemId,
+    required String option,
+    required int count,
+  }) = _CartItem;
 }
