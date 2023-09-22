89364fc498c1a0cb41c33fcfd9e7b272a714a871
Maya Sato <maya-sato@itsol.co.jp>
2023-09-22 14:03:52 +0900

    [update] 買い物籠画面に遷移を実装

--- lib/pages/cart_page/cart_page.dart
+++ lib/pages/cart_page/cart_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  static const path = 'cart';
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
